b47ba3ed3580f9113af4f9c91e064dfc2f7d2ab3
unknown <swkim960715@gmail.com>
2023-08-11 12:04:07 +0900

    Backend first commit

---
+++ src/main/java/com/swk/myapp/MyappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swk.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappBackendApplication.class, args);
+	}
+
+}
